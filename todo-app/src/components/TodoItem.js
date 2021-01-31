ff3eb583c0fd33787dd3b1f8976caa5cf2841c9f
numoonchld <raghavendra.saralaya@gmail.com>
2021-01-31 14:56:13 +0530

    conditional styling

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontColor: "#888",
+        fontStyle: 'italic',
+        textDecoration: 'line-through',
+    }
+
     return (
         <div className="todo-card">
             <input type="checkbox" checked={ props.check} onChange={() => props.clickHandler(props.id)}/>
-            <span> {props.text} </span>
+            <span style={ props.check ? completedStyle : null} > {props.text} </span>
         </div>
     )
 }
